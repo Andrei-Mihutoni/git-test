c6f2e99b548c00608df4eb91ef13d812d234bab9
Andrei-Mihutoni <andrei.mihutoni@gmail.com>
2020-02-04 10:49:02 +0100

    completed bubles

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function start(){
     const bubbles = document.querySelectorAll("#wrapper>div");
 
     console.log(bubbles);
+
+    bubbles.forEach(bubble=>{
+        const x = Math.random()*100;
+        const y = Math.random()*100;
+        const hue = Math.random()*100;
+
+        bubble.style.background="hsl("+hue+", 70%, 70%)";
+        bubble.style.transform="translate("+x+"vw,"+y+"vh)";
+
+    })
+
+}
